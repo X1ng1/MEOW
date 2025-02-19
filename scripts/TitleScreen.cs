f0872f26d958c5f33441c98aa9fec68d0d362375
Ying Jiang <yjiang0229@gmail.com>
2025-02-19 12:10:05 -0500

    made a title screen with background image and start button

---
+++ scripts/TitleScreen.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class TitleScreen : CanvasLayer
+{
+	// Called when the node enters the scene tree for the first time.
+	
+	[Signal]
+	public delegate void StartGameEventHandler();
+	
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void OnStartButtonPressed()
+	{
+		Hide();
+		EmitSignal(SignalName.StartGame);
+
+	}
+}
