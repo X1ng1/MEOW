623ded526226337d3a9383441362773168a56fd3
Ying Jiang <yjiang0229@gmail.com>
2025-03-27 22:15:31 -0400

    added stopping movement

--- scripts/Cat.cs
+++ scripts/Cat.cs
@@ -7,20 +7,27 @@ public partial class Cat : Node2D
 	[Export]
 	public int Speed { get; set; } = 50; // How fast the cats move
 	public Vector2 Direction;
-
 	public Vector2 ScreenSize;
-	private const float MinAngle1 = 325f;  // 325° to 45° range
-    private const float MaxAngle1 = 45f;
-    private const float MinAngle2 = 135f;  // 135° to 225° range
-    private const float MaxAngle2 = 225f;
+	private const float MIN_ANGLE1 = 325f;  // 325° to 45° range
+    private const float MAX_ANGLE1 = 45f;
+    private const float MIN_ANGLE2 = 135f;  // 135° to 225° range
+    private const float MAX_ANGLE2 = 225f;
+	private Random random = new Random();
+	private Timer MovementTimer;
+
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		ScreenSize = GetViewportRect().Size;
-		Position = GetViewportRect().Size / 2;
+		Position = new Vector2(random.Next(0, (int)ScreenSize.X), random.Next(0, (int)ScreenSize.Y));
 		OnMovementTimerTimeOut();
-		
+		MovementTimer = new Timer();
+		AddChild(MovementTimer);
+		MovementTimer.WaitTime = 20;
+		MovementTimer.Timeout += OnMovementTimerTimeOut;
+		MovementTimer.Start();
+
 
 
 	}
@@ -29,7 +36,7 @@ public partial class Cat : Node2D
 	public override void _Process(double delta)
 	{
 		Position += Direction * Speed * (float)delta;
-		
+
 		if (Position.X < 0 || Position.X > ScreenSize.X || Position.Y < 0 || Position.Y > ScreenSize.Y)
 		{
 			Direction = -Direction;
@@ -38,21 +45,29 @@ public partial class Cat : Node2D
 
 	private void OnMovementTimerTimeOut()
 	{
-		GD.Print("sss");
-		Random random = new Random();
-		float angle = 0f;
-		if (random.NextDouble() > 0.5)
+
+		if (random.Next(0, 7) == 0)
 		{
-			angle = (float)(random.NextDouble() * (MaxAngle1 - MinAngle1) + MinAngle1);
+			Speed = 0;
 		}
 		else
 		{
-			angle = (float)(random.NextDouble() * (MaxAngle2 - MinAngle2) + MinAngle2);
+			Speed = 50;
+			float angle;
+			if (random.NextDouble() > 0.5)
+			{
+				angle = (float)(random.NextDouble() * (MAX_ANGLE1 - MIN_ANGLE1) + MIN_ANGLE1);
+			}
+			else
+			{
+				angle = (float)(random.NextDouble() * (MAX_ANGLE2 - MIN_ANGLE2) + MIN_ANGLE2);
+			}
+
+			// Convert angle to radians
+			float angleInRadians = Mathf.Pi / 180f * angle;
+
+			Direction = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians)).Normalized();
 		}
-
-		// Convert angle to radians
-		float angleInRadians = (Mathf.Pi / 180f) * angle;
-
-		Direction = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians)).Normalized();
+		GD.Print(Speed);
 	}
 }
